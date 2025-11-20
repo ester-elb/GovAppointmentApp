fac891b6605093743557320e7bc4ccdeb68d7bec
ester-elb <esti8532@gmail.com>
2025-11-20 02:32:47 +0200

    Add Office model with properties

---
+++ Models/Office.cs
@@ -0,0 +1,10 @@
+namespace AppointmentApi.Models
+{
+    public class Office
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string City { get; set; }
+        public string Address { get; set; }
+    }
+}
