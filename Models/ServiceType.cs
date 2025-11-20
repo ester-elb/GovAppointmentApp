7f23b525b4ce54e63fe829d4ac03faf9ebc5e6a0
ester-elb <esti8532@gmail.com>
2025-11-20 02:33:51 +0200

    Add ServiceType model with properties

---
+++ Models/ServiceType.cs
@@ -0,0 +1,10 @@
+namespace AppointmentApi.Models
+{
+    public class ServiceType
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int DurationMinutes { get; set; }
+    }
+}
