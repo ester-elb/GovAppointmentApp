0c258240ca272214b55963212a26979f726045e1
ester-elb <esti8532@gmail.com>
2025-11-20 09:29:30 +0200

    Refactor Office model for MongoDB integration
    Updated Office model to use MongoDB attributes and changed City to CityCode.

--- Models/Office.cs
+++ Models/Office.cs
@@ -1,10 +1,21 @@
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
 namespace AppointmentApi.Models
 {
     public class Office
     {
-        public string Id { get; set; }
-        public string Name { get; set; }
-        public string City { get; set; }
-        public string Address { get; set; }
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string? Id { get; set; }
+        
+        [BsonElement("Name")]
+        public string? Name { get; set; }
+        
+        [BsonElement("City")]
+        public int? CityCode { get; set; }
+        
+        [BsonElement("Address")]
+        public string? Address { get; set; }
     }
 }
