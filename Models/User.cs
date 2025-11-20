63a352b7857bb8702a3cadd970fb41e8d51e68c3
ester-elb <esti8532@gmail.com>
2025-11-20 09:37:15 +0200

    Change User Id property type from string to int

--- Models/User.cs
+++ Models/User.cs
@@ -2,7 +2,7 @@ namespace AppointmentApi.Models
 {
     public class User
     {
-        public string Id { get; set; }
+        public int Id { get; set; }
         public string FullName { get; set; }
         public string Phone { get; set; }
     }
