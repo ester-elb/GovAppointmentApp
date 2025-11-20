61317ed1de3b6b5628ef42310823963dae399852
ester-elb <esti8532@gmail.com>
2025-11-20 02:33:24 +0200

    Add User model with properties Id, FullName, and Phone

---
+++ Models/User.cs
@@ -0,0 +1,9 @@
+namespace AppointmentApi.Models
+{
+    public class User
+    {
+        public string Id { get; set; }
+        public string FullName { get; set; }
+        public string Phone { get; set; }
+    }
+}
